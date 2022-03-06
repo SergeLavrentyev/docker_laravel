eb8b5a84afe92b19739ba58b275862840b502246
SL <sergelavrentyev@yandex.ru>
2022-03-06 16:19:29 +0400

    update(docker): update php and mysql verions

--- docker/scripts/nginx.sh
+++ docker/scripts/nginx.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 touch /etc/nginx/conf.d/docker.conf
-envsubst '$PROJECT_PATCH, $PHP_HOST' < /etc/nginx/conf.d/docker.conf.template > /etc/nginx/conf.d/docker.conf
+envsubst '$PROJECT_PATCH, $PHP_HOST, $PROJECT_FOLDER' < /etc/nginx/conf.d/docker.conf.template > /etc/nginx/conf.d/docker.conf
 nginx -g "daemon off;"
